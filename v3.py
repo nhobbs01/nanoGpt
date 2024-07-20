38021cfc8938962755c6b0db460a6fe70c0ea390
Nick Hobbs <nickhobbs09@gmail.com>
2024-07-20 10:46:55 +0100

    add v3.py scale up to run on gpu

--- v3.py
+++ v3.py
@@ -2,17 +2,18 @@ import torch
 import torch.nn as nn
 from torch.nn import functional as F
 
+# Scale the model up to run on GPU
 # hyperparameters
-batch_size = 32 # number of independent sequences processed in parallel
-block_size = 8 # context size
+batch_size = 64 # number of independent sequences processed in parallel
+block_size = 256 # context size
 max_iters = 5000
 eval_interval = 500
-learning_rate = 1e-2
+learning_rate = 3e-4
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 eval_iters = 200
-n_embed = 32
-n_head = 4
-n_layer = 4
+n_embed = 384
+n_head = 6
+n_layer = 6
 dropout = 0.2
 # -----------------
 
