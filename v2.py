740924c11c14aa38b0c68a7029bb0bf1f3baf019
Nick Hobbs <nickhobbs09@gmail.com>
2024-07-20 10:39:31 +0100

    add layer norm and addition to the network

--- v2.py
+++ v2.py
@@ -11,7 +11,8 @@ learning_rate = 1e-2
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 eval_iters = 200
 n_embed = 32
-n_heads = 4
+n_head = 4
+n_layer = 4
 # -----------------
 
 torch.manual_seed(1337)
@@ -89,13 +90,15 @@ class Head(nn.Module):
     
 class MultiHead(nn.Module):
 
-    def __init__(self, n_heads, head_size):
+    def __init__(self, n_head, head_size):
         super().__init__()
-        self.heads =nn.ModuleList([Head(head_size) for i in range(n_heads)])
+        self.heads =nn.ModuleList([Head(head_size) for i in range(n_head)])
+        self.proj = nn.Linear(n_embed, n_embed)
 
     def forward(self, x):
         outs = [head(x) for head in self.heads]
         out = torch.cat(outs, -1)
+        out = self.proj(out)
         return out
 
 class FeedForward(nn.Module):
@@ -103,19 +106,37 @@ class FeedForward(nn.Module):
     def __init__(self, n_embed):
         super().__init__()
         self.net = nn.Sequential(
-            nn.Linear(n_embed, n_embed),
-            nn.ReLU()
+            nn.Linear(n_embed, 4*n_embed),
+            nn.ReLU(),
+            nn.Linear(4*n_embed, n_embed),
         )
     def forward(self, x):
         return self.net(x)
+    
+class Block(nn.Module):
+    def __init__(self, n_embed, n_head):
+        super().__init__()
+        head_size = n_embed // n_head
+        self.sa = MultiHead(n_head, head_size)
+        self.ln1 = nn.LayerNorm(n_embed) # Normalize the last dim (C) which is n_embed
+        self.ffwd = FeedForward(n_embed)
+        self.ln2 = nn.LayerNorm(n_embed)
 
+    def forward(self, x):
+        x = self.sa(self.ln1(x))+ x
+        x = self.ffwd(self.ln2(x)) + x
+        return x
+    
 class TransformerModel(nn.Module):
     def __init__(self):
         super().__init__()
         self.token_embedding_table = nn.Embedding(vocab_size, n_embed)
         self.position_embedding_table = nn.Embedding(block_size, n_embed)
-        self.sa_heads = MultiHead(n_heads, n_embed//n_heads)
-        self.ffwd = FeedForward(n_embed)
+        self.blocks = nn.Sequential(
+            Block(n_embed, n_head),
+            Block(n_embed, n_head),
+            Block(n_embed, n_head))
+        self.blocks = nn.Sequential(*[Block(n_embed, n_head) for _ in range(n_layer)])
         self.lm_head = nn.Linear(n_embed, vocab_size)
         
 
@@ -125,8 +146,7 @@ class TransformerModel(nn.Module):
         token_emb = self.token_embedding_table(idx)  # B, T, C  (C is n_embed)
         position_emb = self.position_embedding_table(torch.arange(T, device=device)) # T, C (create an embedding for each time step)
         x = token_emb + position_emb # (B, T, C)
-        x = self.sa_heads(x) # (B, T, C)
-        x = self.ffwd(x) # (B, T, C)
+        x = self.blocks(x)
         logits = self.lm_head(x) # B, T, vocab_size
 
         if targets is None:
@@ -178,13 +198,33 @@ printSampleFromModel(context, 500)
 
 """
 LOG:
+all with these params
+batch_size = 32
+block_size = 8 # context size
+max_iters = 5000
+eval_interval = 500
+learning_rate = 1e-2
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+eval_iters = 200
+n_embed = 32
+n_head = 4
 
 with one self-attention head
-- val loss about 2.4
+- val loss ~2.4
 
 simple multi head attention - 4 heads
- - val loss 2.27
+ - val loss ~2.27
 
 with feed forward layer after multi head attention 
- - val loss 2.23
+ - val loss ~2.23
+
+with layer normalization + residual connections (1 block)
+ - val loss ~2.21
+
+with 4 blocks (multi-head self attention, norm + add, feed forward)
+ - val loss  ~2.22
+
+with applying the norm before the transformation (this is a deviation from the paper but how it is done now)
+- val loss ~2.14
+
 """
